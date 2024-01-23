a40087cc9d04cabec493912b6cd0b47636cc13c7
Alejandro Morales Sierra <alejandro.moralessierra@wur.nl>
2024-01-23 11:15:25 +0100

    Document use of `shading = false`

--- src/Render.jl
+++ src/Render.jl
@@ -66,7 +66,9 @@ Render a `Scene` object. This will create a new visualization (see
 Documentation for details). `normals = true` will draw arrows in the direction
 of the normal vector for each triangle in the mesh, `wireframe = true` will draw
 the edges of each triangle with black lines. Keyword arguments are passed to
-`Makie.mesh()`.
+`Makie.mesh()`. The actual color of each triangle depends on the illumination of the scene
+but it is possible to turn this off by passing `shading = false`. This will use the exact
+colors specified in the `Scene` object.
 """
 function render(scene::Scene; normals::Bool = false, wireframe::Bool = false, kwargs...)
     render(
